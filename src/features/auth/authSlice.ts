30df0c6546093d1a0166c31a4a2d2135c4a3ff43
Sergey <santerakis@gmail.com>
2023-05-24 21:40:30 +0300

    addCase-rejected

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,19 +1,30 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { ArgLoginType, ArgRegisterType, authApi, ProfileType } from "./authApi";
 
-const register = createAsyncThunk("auth/register", (arg: ArgRegisterType, thunkAPI) => {
-  // const { dispatch, getState, rejectWithValue } = thunkAPI;
-  authApi.register(arg).then((res) => {
-    console.log("res: ", res);
-  });
+// const register = createAsyncThunk("auth/register", (arg: ArgRegisterType, thunkAPI) => {
+//   const { dispatch, getState, rejectWithValue } = thunkAPI;
+//   authApi.register(arg).then((res) => {
+//     console.log("res: ", res);
+//   });
+// });
+
+const register = createAsyncThunk("auth/register", async (arg: ArgRegisterType, thunkAPI) => {
+  debugger
+  await authApi.register(arg);
 });
 
-const login = createAsyncThunk("auth/login", (arg: ArgLoginType, thunkAPI) => {
-  // const { dispatch } = thunkAPI;
-  return authApi.login(arg).then((res) => {
-    // dispatch(authActions.setProfile({ profile: res.data }));
-    return { profile: res.data };
-  });
+// const login = createAsyncThunk("auth/login", (arg: ArgLoginType, thunkAPI) => {
+//   const { dispatch } = thunkAPI;
+//   return authApi.login(arg).then((res) => {
+//     dispatch(authActions.setProfile({ profile: res.data }));
+//     return { profile: res.data };
+//   });
+// });
+
+// async()=>{} - всегда возвращает промиc
+const login = createAsyncThunk("auth/login", async (arg: ArgLoginType, thunkAPI) => {
+  const res = await authApi.login(arg);
+  return { profile: res.data };
 });
 
 const slice = createSlice({
@@ -27,9 +38,13 @@ const slice = createSlice({
     // }
   },
   extraReducers: builder => {
-    builder.addCase(login.fulfilled, (state, action) => {
-      state.profile = action.payload.profile;
-    });
+    builder
+      .addCase(login.fulfilled, (state, action) => {
+        state.profile = action.payload.profile;
+      })
+      // .addCase(register.rejected, (state, action) => {
+      //   debugger
+      // });
   }
 });
 
