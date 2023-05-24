6b9d0d561922f6e393111cc09f46ff32243c426c
Sergey <santerakis@gmail.com>
2023-05-24 20:24:32 +0300

    addCase_then

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,25 +1,39 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { ArgLoginType, ArgRegisterType, authApi } from "./authApi";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ArgLoginType, ArgRegisterType, authApi, ProfileType } from "./authApi";
 
 const register = createAsyncThunk("auth/register", (arg: ArgRegisterType, thunkAPI) => {
   // const { dispatch, getState, rejectWithValue } = thunkAPI;
   authApi.register(arg).then((res) => {
-    console.log('res: ',res)
+    console.log("res: ", res);
   });
 });
 
 const login = createAsyncThunk("auth/login", (arg: ArgLoginType, thunkAPI) => {
-  authApi.login(arg).then((res) => {
-    console.log('login res: ', res.data)
-  })
-})
+  // const { dispatch } = thunkAPI;
+  return authApi.login(arg).then((res) => {
+    // dispatch(authActions.setProfile({ profile: res.data }));
+    return { profile: res.data };
+  });
+});
 
 const slice = createSlice({
   name: "auth",
-  initialState: {},
-  reducers: {}
+  initialState: {
+    profile: null as ProfileType | null
+  },
+  reducers: {
+    // setProfile: (state, action: PayloadAction<{ profile: ProfileType }>) => {
+    //   state.profile = action.payload.profile;
+    // }
+  },
+  extraReducers: builder => {
+    builder.addCase(login.fulfilled, (state, action) => {
+      state.profile = action.payload.profile;
+    });
+  }
 });
 
-export const authReducer = slice.reducer
-export const authThunks = {register, login}    //все санки упаковываем в объект
+export const authReducer = slice.reducer;
+// export const authActions = slice.actions;
+export const authThunks = { register, login };    //все санки упаковываем в объект
 
